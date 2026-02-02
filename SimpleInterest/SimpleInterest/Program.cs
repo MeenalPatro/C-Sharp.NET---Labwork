e889778bc9b6a19bc81508584f71a100e6985a6c
Meenal Patro <meenalpatro@gmail.com>
2026-02-02 18:44:33 +0530

    Add files via upload

---
+++ SimpleInterest/SimpleInterest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Application
+{
+     class SimpleInterest
+    {
+        static void Main(string[] args)
+        {
+            int p= int.Parse(Console.ReadLine());
+			int r = int.Parse(Console.ReadLine());
+			int n = int.Parse(Console.ReadLine());
+            int si = p * r * n / 100;
+            Console.WriteLine("Principal amount is : " + (p));
+			Console.WriteLine("Rate of interest is : " + (r));
+			Console.WriteLine("Number of years is : " + (n));
+			Console.WriteLine("Simple Interest is : " + (si));
+
+		}
+    }
+}
