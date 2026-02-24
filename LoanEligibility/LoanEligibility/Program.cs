68ddac1da7f1e52fc097fe2529dfd6843b51e68d
Meenal Patro <meenalpatro@gmail.com>
2026-02-24 08:15:48 +0530

    Add files via upload

---
+++ LoanEligibility/LoanEligibility/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LoanEligibility
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int age, creditScore;
+			double income;
+
+			Console.Write("Enter Age: ");
+			age = int.Parse(Console.ReadLine());
+
+			Console.Write("Enter Monthly Income: ");
+			income = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter Credit Score: ");
+			creditScore = int.Parse(Console.ReadLine());
+
+			if (age >= 21)
+			{
+				if (income >= 25000)
+				{
+					if (creditScore >= 650)
+					{
+						Console.WriteLine("Loan Approved");
+					}
+					else
+					{
+						Console.WriteLine("Loan Rejected: Low Credit Score");
+					}
+				}
+				else
+				{
+					Console.WriteLine("Loan Rejected: Insufficient Income");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Loan Rejected: Age Below Requirement");
+			}
+		}
+	}
+}
