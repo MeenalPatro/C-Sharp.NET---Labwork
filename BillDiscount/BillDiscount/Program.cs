5518bd148b23dcebc44492c71d97c316d461b53e
Meenal Patro <meenalpatro@gmail.com>
2026-02-24 08:14:54 +0530

    Add files via upload

---
+++ BillDiscount/BillDiscount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BillDiscount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double billAmount, discount = 0;
+			string category;
+
+			Console.Write("Enter Bill Amount: ");
+			billAmount = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter Customer Category (Senior/Regular/Industrial): ");
+			category = Console.ReadLine().ToLower();
+
+			if (category == "senior")
+			{
+				discount = billAmount * 0.20;
+			}
+			else if (category == "regular")
+			{
+				discount = billAmount * 0.10;
+			}
+			else if (category == "industrial")
+			{
+				discount = billAmount * 0.05;
+			}
+			else
+			{
+				Console.WriteLine("Invalid Category");
+			}
+
+			double finalAmount = billAmount - discount;
+			Console.WriteLine("Discount: " + discount);
+			Console.WriteLine("Final Bill Amount: " + finalAmount);
+		}
+	}
+}
