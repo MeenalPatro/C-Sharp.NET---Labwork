8e4fdf2830ff54f58b10e1ef20fc2ab4621cab6b
Meenal Patro <meenalpatro@gmail.com>
2026-02-24 08:17:10 +0530

    Add files via upload

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ATM
+{
+	class ATM
+	{
+		static void Main()
+		{
+			int pin = 1234, enteredPin;
+			double balance = 10000, withdrawAmount;
+
+			Console.Write("Enter PIN: ");
+			enteredPin = int.Parse(Console.ReadLine());
+
+			if (enteredPin == pin)
+			{
+				Console.Write("Enter Withdrawal Amount: ");
+				withdrawAmount = double.Parse(Console.ReadLine());
+
+				if (withdrawAmount <= balance)
+				{
+					balance = balance - withdrawAmount;
+					Console.WriteLine("Withdrawal Successful");
+					Console.WriteLine("Remaining Balance: " + balance);
+				}
+				else
+				{
+					Console.WriteLine("Insufficient Balance");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Invalid PIN");
+			}
+		}
+	}
+}
