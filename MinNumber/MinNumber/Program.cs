28e5b73df1c60ef815ee2cf3125a0aadccfb5c65
Meenal Patro <meenalpatro@gmail.com>
2026-03-02 09:36:45 +0530

    Add files via upload

---
+++ MinNumber/MinNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinNumber
+{
+	class Program
+	{
+		static void Main()
+		{
+			int[] arr = new int[5];
+
+			Console.WriteLine("Enter 5 numbers:");
+
+			for (int i = 0; i < 5; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			int min = arr[0];
+
+			for (int i = 1; i < 5; i++)
+			{
+				if (arr[i] < min)
+				{
+					min = arr[i];
+				}
+			}
+
+			Console.WriteLine("Minimum number is: " + min);
+		}
+	}
+}
