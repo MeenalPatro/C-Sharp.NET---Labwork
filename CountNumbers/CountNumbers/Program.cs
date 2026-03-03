60f2620526d234501d258494a43328b2e619faf9
Meenal Patro <meenalpatro@gmail.com>
2026-03-03 08:07:30 +0530

    Add files via upload

---
+++ CountNumbers/CountNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CountNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[5];
+			int positiveCount = 0, negativeCount = 0;
+
+			Console.WriteLine("Enter 5 numbers:");
+			for (int i = 0; i < 5; i++)
+			{
+				numbers[i] = int.Parse(Console.ReadLine());
+			}
+
+			for (int i = 0; i < 5; i++)
+			{
+				if (numbers[i] > 0)
+					positiveCount++;
+				else if (numbers[i] < 0)
+					negativeCount++;
+			}
+
+			Console.WriteLine("Positive numbers count: " + positiveCount);
+			Console.WriteLine("Negative numbers count: " + negativeCount);
+		}
+	}
+}
