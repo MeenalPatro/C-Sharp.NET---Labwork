ce619735d7939f34e0533df9abad9a303ad6fdf7
Meenal Patro <meenalpatro@gmail.com>
2026-03-15 22:24:31 +0530

    Add files via upload

---
+++ ArrayMax/ArrayMax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayMax
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = new int[5];
+
+			Console.WriteLine("Enter 5 numbers:");
+
+			for (int i = 0; i < 5; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			int max = arr[0];
+
+			for (int i = 1; i < 5; i++)
+			{
+				if (arr[i] > max)
+					max = arr[i];
+			}
+
+			Console.WriteLine("Maximum = " + max);
+		}
+	}
+}
