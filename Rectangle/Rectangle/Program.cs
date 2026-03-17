84df54ca21a9c31be2f3e6abb75e42641bf63b2d
Meenal Patro <meenalpatro@gmail.com>
2026-03-17 08:02:08 +0530

    Add files via upload

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Rectangle
+{
+	int length;
+	int width;
+
+	public Rectangle()
+	{
+		length = 0;
+		width = 0;
+	}
+
+	public Rectangle(int l, int w)
+	{
+		length = l;
+		width = w;
+	}
+
+	public int Area()
+	{
+		return length * width;
+	}
+
+	public void Display()
+	{
+		Console.WriteLine("Length: " + length);
+		Console.WriteLine("Width: " + width);
+		Console.WriteLine("Area: " + Area());
+	}
+}
+
+class Program
+{
+	static void Main(string [] args)
+	{
+		Rectangle r1 = new Rectangle();
+		Console.WriteLine("Rectangle using Default Constructor:");
+		r1.Display();
+
+		Console.WriteLine();
+
+		Rectangle r2 = new Rectangle(10, 5);
+		Console.WriteLine("Rectangle using Parameterized Constructor:");
+		r2.Display();
+	}
+}
