57149fcc4c6cf8fc6a5f1aca5e7a2219eff99cbe
Meenal Patro <meenalpatro@gmail.com>
2026-03-17 08:03:11 +0530

    Add files via upload

---
+++ Student/Student/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Student
+{
+	int rollNo;
+	string name;
+	int marks;
+
+	public Student()
+	{
+		rollNo = 0;
+		name = "Meenal";
+		marks = 0;
+	}
+	public Student(int r, string n, int m)
+	{
+		rollNo = r;
+		name = n;
+		marks = m;
+	}
+
+	public string CalculateGrade()
+	{
+		if (marks >= 90)
+			return "A";
+		else if (marks >= 75)
+			return "B";
+		else if (marks >= 50)
+			return "C";
+		else
+			return "F";
+	}
+	public void Display()
+	{
+		Console.WriteLine("Roll No: " + rollNo);
+		Console.WriteLine("Name: " + name);
+		Console.WriteLine("Marks: " + marks);
+		Console.WriteLine("Grade: " + CalculateGrade());
+	}
+}
+
+class Program
+{
+	static void Main()
+	{
+		Student s1 = new Student();
+		Console.WriteLine("Student using Default Constructor:");
+		s1.Display();
+
+		Console.WriteLine();
+
+		Student s2 = new Student(101, "Meenal", 95);
+		Console.WriteLine("Student using Parameterized Constructor:");
+		s2.Display();
+	}
+}
