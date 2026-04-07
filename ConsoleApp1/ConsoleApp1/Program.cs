d41a43ecfbfb4d45e37f1a74283cf4f4e022d07f
Meenal Patro <meenalpatro@gmail.com>
2026-04-07 08:37:48 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string [] args)
+	{
+		try
+		{
+			Console.Write("Enter a number: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			int result = 100 / num;
+
+			Console.WriteLine("Result: " + result);
+		}
+		catch (DivideByZeroException ex)
+		{
+			Console.WriteLine("Error: Cannot divide by zero.");
+			Console.WriteLine("Message: " + ex.Message);
+		}
+		catch (FormatException ex)
+		{
+			Console.WriteLine("Error: Invalid input format.");
+			Console.WriteLine("Message: " + ex.Message);
+		}
+	}
+}
