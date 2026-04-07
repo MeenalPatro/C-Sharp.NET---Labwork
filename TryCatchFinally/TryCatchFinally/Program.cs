8ed6cb4fc4b4fd373244505d3537e5cb1fa2d14e
Meenal Patro <meenalpatro@gmail.com>
2026-04-07 08:41:01 +0530

    Add files via upload

---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string [] args)
+	{
+		try
+		{
+			Console.Write("Enter a number: ");
+			int num = Convert.ToInt32(Console.ReadLine());
+
+			int result = 50 / num;
+
+			Console.WriteLine("Result: " + result);
+		}
+		catch (DivideByZeroException ex)
+		{
+			Console.WriteLine("Error: Cannot divide by zero.");
+		}
+		catch (FormatException ex)
+		{
+			Console.WriteLine("Error: Invalid input.");
+		}
+		finally
+		{
+			Console.WriteLine("This block always executes.");
+		}
+
+		Console.WriteLine("Program continues...");
+	}
+}
